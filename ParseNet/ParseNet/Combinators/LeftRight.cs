1c4c8a7d3673706f48a5237c869af5ce222519a8
Kohei Hakoishi <wipiano@gmail.com>
2017-11-15 01:27:51 +0900

    add Left(), Right()

---
+++ ParseNet/ParseNet/Combinators/LeftRight.cs
@@ -0,0 +1,47 @@
+﻿using static ParseNet.Functions;
+
+namespace ParseNet.Combinators
+{
+    public static partial class Combinators
+    {
+        public static Parser<TLeft> Left<TLeft, TRight>(this Parser<TLeft> left, Parser<TRight> right)
+        {
+            ParseResult<TLeft> LeftImpl(string source, int position)
+            {
+                var leftResult = left(source, position);
+                if (leftResult.IsSuccess)
+                {
+                    var rightResult = right(source, leftResult.NextPosition);
+
+                    return rightResult.IsSuccess
+                        ? Success(source, rightResult.NextPosition, leftResult.Result)
+                        : Failed<TLeft>(source, position, rightResult.Message);
+                }
+
+                return Failed<TLeft>(source, position, leftResult.Message);
+            }
+
+            return LeftImpl;
+        }
+
+        public static Parser<TRight> Right<TLeft, TRight>(this Parser<TLeft> left, Parser<TRight> right)
+        {
+            ParseResult<TRight> RightImpl(string source, int position)
+            {
+                var leftResult = left(source, position);
+                if (leftResult.IsSuccess)
+                {
+                    var rightResult = right(source, leftResult.NextPosition);
+
+                    return rightResult.IsSuccess
+                        ? rightResult
+                        : Failed<TRight>(source, position, rightResult.Message);
+                }
+
+                return Failed<TRight>(source, position, leftResult.Message);
+            }
+
+            return RightImpl;
+        }
+    }
+}
