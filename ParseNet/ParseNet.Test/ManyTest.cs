e10dfa03e31b70ac6db847a7198eff3ea0ca6951
Kohei Hakoishi <wipiano@gmail.com>
2017-11-15 01:12:58 +0900

    add Many()

---
+++ ParseNet/ParseNet.Test/ManyTest.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Immutable;
+using ParseNet.Combinators;
+using Xunit;
+using static ParseNet.Functions;
+using static ParseNet.StringParsers;
+
+namespace ParseNet.Test
+{
+    public class ManyTest
+    {
+        [Fact]
+        public void StringManyTest()
+        {
+            var hogeParser = Literal("hoge");
+
+            var threetimes = hogeParser.Many().Parse("hogehogehogefugahoge");
+            threetimes.IsSuccess.IsTrue();
+            threetimes.Result.Is("hogehogehoge");
+
+            var empty = hogeParser.Many().Parse("xxxhoge");
+            empty.IsSuccess.IsTrue();
+            empty.Result.Is(string.Empty);
+        }
+
+        [Fact]
+        public void ObjectManyTest()
+        {
+            var hogeParser = Literal("hoge");
+
+            var threetimes = hogeParser.Many<string>().Parse("hogehogehogefugahoge");
+            threetimes.IsSuccess.IsTrue();
+            threetimes.Result.Is("hoge", "hoge", "hoge");
+
+            var empty = hogeParser.Many<string>().Parse("xxxhoge");
+            empty.IsSuccess.IsTrue();
+            empty.Result.Is(ImmutableList<string>.Empty);
+        }
+    }
+}
---
+++ ParseNet/ParseNet/Combinators/Many.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Immutable;
+using static ParseNet.Functions;
+
+namespace ParseNet.Combinators
+{
+    public static partial class Combinators
+    {
+        public static Parser<ImmutableList<T>> Many<T>(this Parser<T> parser)
+        {
+            ParseResult<ImmutableList<T>> ParserImpl(string source, int position, ImmutableList<T> results)
+            {
+                var result = parser(source, position);
+
+                return result.IsSuccess
+                    ? ParserImpl(source, result.NextPosition, results.Add(result.Result))
+                    : Success(source, position, results);
+            }
+
+            return (source, position) => ParserImpl(source, position, ImmutableList<T>.Empty);
+        }
+
+        public static Parser<string> Many(this Parser<string> parser)
+        {
+            ParseResult<string> ParserImpl(string source, int position, string results)
+            {
+                var result = parser(source, position);
+
+                return result.IsSuccess
+                    ? ParserImpl(source, result.NextPosition, results + result.Result)
+                    : Success(source, position, results);
+            }
+
+            return (source, position) => ParserImpl(source, position, string.Empty);
+        }
+    }
+}
